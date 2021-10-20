1f8cacf27b2b009b729b479eb02cb352bd2374e8
Muhammad Irfani <muhammadirfani340@gmail.com>
2021-10-20 19:58:27 +0700

    update tugas

---
+++ module2/tugas/BigInteger.java
@@ -0,0 +1,10 @@
+package module2.tugas;
+
+// disini error karena harus menambahkan huruf L di akhir sebelum petik koma, karena data tsb merupakan data Long
+public class BigInteger {
+    public static void main(String[] args) {
+        long p = 2147483648L;
+        System.out.println(p);
+    }
+
+}
